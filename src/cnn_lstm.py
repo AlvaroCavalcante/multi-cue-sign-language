b6072fefc15356fc186045d82728569dcafd6647
Alvaro Carneiro <leandro0807@live.com>
2022-04-19 23:35:30 -0300

    adding learning rate scheduler and reducing the img dim

--- src/cnn_lstm.py
+++ src/cnn_lstm.py
@@ -1,10 +1,14 @@
+import pickle
+
 import tensorflow as tf
+import pandas as pd
 from tensorflow import keras
-from tensorflow.keras.models import Sequential, Model
+from tensorflow.keras.models import Model
 from tensorflow.keras.layers import Concatenate, Conv2D, MaxPooling2D, GlobalAveragePooling2D, TimeDistributed, Flatten
-from tensorflow.keras.callbacks import ModelCheckpoint
+from tensorflow.keras.callbacks import ModelCheckpoint, LearningRateScheduler
 
 from read_dataset import load_data_tfrecord
+import lr_scheduler
 
 physical_devices = tf.config.list_physical_devices('GPU')
 try:
@@ -15,8 +19,8 @@ except:
 MAX_SEQ_LENGTH = 16
 NUM_FEATURES = 3000
 NUMBER_OF_CLASSES = 3
-HAND_WIDTH, HAND_HEIGHT = 80, 80
-FACE_WIDTH, FACE_HEIGHT = 80, 80
+HAND_WIDTH, HAND_HEIGHT = 50, 50
+FACE_WIDTH, FACE_HEIGHT = 50, 50
 
 def get_sequence_model():
     cnn_model = get_cnn_model()
@@ -36,10 +40,10 @@ def get_sequence_model():
     rnn_model = keras.Model(frame_features_input, output)
 
     rnn_model.compile(
-        loss="sparse_categorical_crossentropy", optimizer=tf.keras.optimizers.Adam(learning_rate=0.001), metrics=["accuracy"]
+        loss="sparse_categorical_crossentropy", optimizer=tf.keras.optimizers.Adam(learning_rate=0.01), metrics=["accuracy"]
     )
 
-    tf.keras.utils.plot_model(rnn_model, "model.png", show_shapes=True)
+    # tf.keras.utils.plot_model(rnn_model, "model.png", show_shapes=True)
     return rnn_model
 
 
@@ -67,7 +71,7 @@ def get_cnn_model():
     final_output = Concatenate()([concat_layers, triangle_input])
 
     model = Model(inputs=[hand1_input, hand2_input, face_input, triangle_input], outputs=final_output)
-    tf.keras.utils.plot_model(model, "model.png", show_shapes=True)
+    # tf.keras.utils.plot_model(model, "model.png", show_shapes=True)
 
     return model
 
@@ -79,8 +83,7 @@ def count_data_items(tfrecord):
 
     return count
 
-train_files = ['/home/alvaro/Documentos/video2tfrecord/example/train/batch_1_of_2.tfrecords',
-'/home/alvaro/Documentos/video2tfrecord/example/train/batch_2_of_2.tfrecords']
+train_files = tf.io.gfile.glob('./src/data/*.tfrecords')
 
 batch_size = 6
 
@@ -90,16 +93,24 @@ num_training_imgs = count_data_items(train_files)
 
 train_steps = num_training_imgs // batch_size
 
+a_file = open("./src/data.pkl", "rb")
+rep_dict = pickle.load(a_file)
+rep_dict = dict((v, k) for k, v in rep_dict.items())
+
 def train_gen():
-    rep_dict = {104: 0, 1: 1, 118: 2 }
-    for (hand_seq, face_seq, triangle_data, centroids, video_imgs, label, video_name_list, triangle_stream) in dataset:
-        yield [hand_seq[:, 0], hand_seq[:, 1], face_seq, triangle_data], tf.constant([rep_dict[x.numpy()] for x in label], dtype=tf.int8)
+    rep_dict = {104: 2, 1: 1, 118: 2 }
+    for (hand_seq, face_seq, triangle_data, centroids, label, video_name_list, triangle_stream) in dataset:
+        yield [hand_seq[:, 0], hand_seq[:, 1], face_seq, triangle_data], tf.constant([rep_dict[x.numpy()] for x in label], dtype=tf.int16)
 
 callbacks_list = [
     ModelCheckpoint('src/model', monitor='accuracy', verbose=1, save_best_only=True),
+    LearningRateScheduler(lr_scheduler.lr_time_based_decay, verbose=1)
 ]
 
-get_sequence_model().fit(train_gen(),
+result = get_sequence_model().fit(train_gen(),
                     steps_per_epoch=train_steps,
-                    epochs=85,
+                    epochs=80,
                     callbacks=callbacks_list)
+
+history_frame = pd.DataFrame(result.history)
+history_frame.to_csv('src/history.csv', index=False)
