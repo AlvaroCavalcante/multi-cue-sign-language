66b15092c38f458b23ff34aa6f27c93f6b0c7cf8
Alvaro Carneiro <leandro0807@live.com>
2022-04-22 01:57:51 -0300

    passing variables dinamically

--- src/cnn_lstm.py
+++ src/cnn_lstm.py
@@ -1,4 +1,4 @@
-import pickle
+import argparse
 
 import tensorflow as tf
 import pandas as pd
@@ -16,11 +16,17 @@ try:
 except:
     print('GPU not found')
 
+
+
 MAX_SEQ_LENGTH = 16
 NUM_FEATURES = 3000
-NUMBER_OF_CLASSES = 3
+NUMBER_OF_CLASSES = 226
 HAND_WIDTH, HAND_HEIGHT = 50, 50
 FACE_WIDTH, FACE_HEIGHT = 50, 50
+TRAIN_FILES = 'TEST'
+BATCH_SIZE = 6
+EPOCHS = 80
+
 
 def get_sequence_model():
     cnn_model = get_cnn_model()
@@ -94,24 +100,17 @@ def count_data_items(tfrecord):
 
     return count
 
-train_files = tf.io.gfile.glob('./src/data/*.tfrecords')
 
-batch_size = 6
+dataset = load_data_tfrecord(TRAIN_FILES, BATCH_SIZE)
 
-dataset = load_data_tfrecord(train_files, batch_size)
+num_training_imgs = count_data_items(TRAIN_FILES)
 
-num_training_imgs = count_data_items(train_files)
+train_steps = num_training_imgs // BATCH_SIZE
 
-train_steps = num_training_imgs // batch_size
-
-a_file = open("./src/data.pkl", "rb")
-rep_dict = pickle.load(a_file)
-rep_dict = dict((v, k) for k, v in rep_dict.items())
 
 def train_gen():
-    rep_dict = {104: 2, 1: 1, 118: 2 }
     for (hand_seq, face_seq, triangle_data, centroids, label, video_name_list, triangle_stream) in dataset:
-        yield [hand_seq[:, 0], hand_seq[:, 1], face_seq, triangle_data], tf.constant([rep_dict[x.numpy()] for x in label], dtype=tf.int16)
+        yield [hand_seq[:, 0], hand_seq[:, 1], face_seq, triangle_data], label
 
 callbacks_list = [
     ModelCheckpoint('src/model', monitor='accuracy', verbose=1, save_best_only=True),
@@ -120,7 +119,7 @@ callbacks_list = [
 
 result = get_sequence_model().fit(train_gen(),
                     steps_per_epoch=train_steps,
-                    epochs=80,
+                    epochs=EPOCHS,
                     callbacks=callbacks_list)
 
 history_frame = pd.DataFrame(result.history)
