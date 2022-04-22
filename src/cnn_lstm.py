24cbf90a430d2fa9bbd480d4e0be8540dd7e8ecc
Alvaro Carneiro <leandro0807@live.com>
2022-04-22 02:08:22 -0300

    identation and creating method to train

--- src/cnn_lstm.py
+++ src/cnn_lstm.py
@@ -16,22 +16,24 @@ try:
 except:
     print('GPU not found')
 
-
+parser = argparse.ArgumentParses(description='Train a CNN-LSTM model')
+parser.add_argument('--batch_size', type=int, help='Batch size')
+parser.add_argument('--epochs', type=int, help='Number of epochs')
+args = parser.parse_args()
 
 MAX_SEQ_LENGTH = 16
-NUM_FEATURES = 3000
 NUMBER_OF_CLASSES = 226
 HAND_WIDTH, HAND_HEIGHT = 50, 50
 FACE_WIDTH, FACE_HEIGHT = 50, 50
-TRAIN_FILES = 'TEST'
-BATCH_SIZE = 6
-EPOCHS = 80
+BATCH_SIZE = args.batch_size
+EPOCHS = args.epochs
 
 
 def get_sequence_model():
     cnn_model = get_cnn_model()
 
-    frame_features_input = [keras.Input((16, HAND_WIDTH, HAND_HEIGHT, 3), name="input"+str(c)) for c in range(3)]
+    frame_features_input = [keras.Input(
+        (16, HAND_WIDTH, HAND_HEIGHT, 3), name="input"+str(c)) for c in range(3)]
     frame_features_input.append(keras.Input((16, 13), name='triangle_data'))
 
     # Refer to the following tutorial to understand the significance of using `mask`:
@@ -56,29 +58,36 @@ def get_sequence_model():
 
 def get_hand_sequence(input_1, input_2):
     merged = Concatenate()([input_1, input_2])
-    x = Conv2D(filters=82, kernel_size=3, padding="same",activation="relu")(merged)
+    x = Conv2D(filters=82, kernel_size=3,
+               padding="same", activation="relu")(merged)
     x = MaxPooling2D(pool_size=2)(x)
-    x = Conv2D(filters=42, kernel_size=3, padding="same",activation="relu")(x)
+    x = Conv2D(filters=42, kernel_size=3, padding="same", activation="relu")(x)
     x = MaxPooling2D(pool_size=2)(x)
-    x = Conv2D(filters=16, kernel_size=3, padding="same",activation="relu")(x)
+    x = Conv2D(filters=16, kernel_size=3, padding="same", activation="relu")(x)
     x = MaxPooling2D(pool_size=2)(x)
     output = GlobalAveragePooling2D()(x)
     return output
+
 
 def get_face_sequence(face_input):
-    x = Conv2D(filters=64, kernel_size=3, padding="same",activation="relu")(face_input)
+    x = Conv2D(filters=64, kernel_size=3, padding="same",
+               activation="relu")(face_input)
     x = MaxPooling2D(pool_size=2)(x)
-    x = Conv2D(filters=32, kernel_size=3, padding="same",activation="relu")(x)
+    x = Conv2D(filters=32, kernel_size=3, padding="same", activation="relu")(x)
     x = MaxPooling2D(pool_size=2)(x)
-    x = Conv2D(filters=16, kernel_size=3, padding="same",activation="relu")(x)
+    x = Conv2D(filters=16, kernel_size=3, padding="same", activation="relu")(x)
     x = MaxPooling2D(pool_size=2)(x)
     output = GlobalAveragePooling2D()(x)
     return output
 
+
 def get_cnn_model():
-    hand1_input = tf.keras.layers.Input(shape=(HAND_WIDTH, HAND_HEIGHT, 3), name='hand1_input')
-    hand2_input = tf.keras.layers.Input(shape=(HAND_WIDTH, HAND_HEIGHT, 3), name='hand2_input')
-    face_input = tf.keras.layers.Input(shape=(FACE_WIDTH, FACE_HEIGHT, 3), name='face_input')
+    hand1_input = tf.keras.layers.Input(
+        shape=(HAND_WIDTH, HAND_HEIGHT, 3), name='hand1_input')
+    hand2_input = tf.keras.layers.Input(
+        shape=(HAND_WIDTH, HAND_HEIGHT, 3), name='hand2_input')
+    face_input = tf.keras.layers.Input(
+        shape=(FACE_WIDTH, FACE_HEIGHT, 3), name='face_input')
     triangle_input = tf.keras.layers.Input(shape=(13), name='triangle_input')
 
     hand_seq = get_hand_sequence(hand1_input, hand2_input)
@@ -87,40 +96,44 @@ def get_cnn_model():
     concat_layers = Concatenate()([hand_seq, face_seq])
     final_output = Concatenate()([concat_layers, triangle_input])
 
-    model = Model(inputs=[hand1_input, hand2_input, face_input, triangle_input], outputs=final_output)
+    model = Model(inputs=[hand1_input, hand2_input,
+                  face_input, triangle_input], outputs=final_output)
     # tf.keras.utils.plot_model(model, "model.png", show_shapes=True)
 
     return model
 
+
 def count_data_items(tfrecord):
     count = 0
     for fn in tfrecord:
-      for _ in tf.compat.v1.python_io.tf_record_iterator(fn):
-        count += 1
+        for _ in tf.compat.v1.python_io.tf_record_iterator(fn):
+            count += 1
 
     return count
 
 
-dataset = load_data_tfrecord(TRAIN_FILES, BATCH_SIZE)
-
-num_training_imgs = count_data_items(TRAIN_FILES)
-
-train_steps = num_training_imgs // BATCH_SIZE
-
-
-def train_gen():
+def train_gen(dataset):
     for (hand_seq, face_seq, triangle_data, centroids, label, video_name_list, triangle_stream) in dataset:
         yield [hand_seq[:, 0], hand_seq[:, 1], face_seq, triangle_data], label
 
-callbacks_list = [
-    ModelCheckpoint('src/model', monitor='accuracy', verbose=1, save_best_only=True),
-    LearningRateScheduler(lr_scheduler.lr_time_based_decay, verbose=1)
-]
 
-result = get_sequence_model().fit(train_gen(),
-                    steps_per_epoch=train_steps,
-                    epochs=EPOCHS,
-                    callbacks=callbacks_list)
+def train_cnn_lstm_model(train_files):
+    dataset = load_data_tfrecord(train_files, BATCH_SIZE)
 
-history_frame = pd.DataFrame(result.history)
-history_frame.to_csv('src/history.csv', index=False)
+    num_training_imgs = count_data_items(train_files)
+
+    train_steps = num_training_imgs // BATCH_SIZE
+
+    callbacks_list = [
+        ModelCheckpoint('src/model', monitor='accuracy',
+                        verbose=1, save_best_only=True),
+        LearningRateScheduler(lr_scheduler.lr_time_based_decay, verbose=1)
+    ]
+
+    result = get_sequence_model().fit(train_gen(dataset),
+                                      steps_per_epoch=train_steps,
+                                      epochs=EPOCHS,
+                                      callbacks=callbacks_list)
+
+    history_frame = pd.DataFrame(result.history)
+    history_frame.to_csv('src/history.csv', index=False)
