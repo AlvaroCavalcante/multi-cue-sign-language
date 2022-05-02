231097fec95a87e66a097f8464ce61625b0005f1
Alvaro Carneiro <leandro0807@live.com>
2022-05-02 09:38:44 -0300

    improving decay rate

--- src/utils/lr_scheduler.py
+++ src/utils/lr_scheduler.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 
 def lr_time_based_decay(epoch, lr, nb_epoch=0):
     # decay = lr / nb_epoch
-    decay = 0.0018
+    decay = 0.007
     return lr * 1 / (1 + decay * epoch)
 
 
@@ -45,7 +45,7 @@ def plot_lr_decay(lr_function, lr, epoch, nb_epoch):
         return plot_lr_decay(lr_function, lr, epoch+1, nb_epoch)
 
 if __name__ == "__main__":
-    nb_epoch = 50
+    nb_epoch = 20
     returned_lr = plot_lr_decay(lr_time_based_decay, [0.01], 0, nb_epoch)
     print(returned_lr)
 
