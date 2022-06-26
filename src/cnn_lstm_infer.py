b77dbb69d48c4f85aaaee642b9dfebd244b08e57
alvaro leandro <leandro0807@live.com>
2022-06-26 19:39:02 -0300

    updated script to evaluate model

--- src/cnn_lstm_infer.py
+++ src/cnn_lstm_infer.py
@@ -1,20 +1,39 @@
 import numpy as np
 import pandas as pd
 from tensorflow.keras.models import load_model
+import tensorflow as tf
 
 from read_dataset import load_data_tfrecord
+import cnn_lstm
 
-dataset = load_data_tfrecord('./src/batch_1_of_98_2022-04-19-01-04-1650330291.tfrecords', 1)
+tf.config.set_visible_devices([], 'GPU')
+visible_devices = tf.config.get_visible_devices()
+for device in visible_devices:
+    assert device.device_type != 'GPU'
+
+files = tf.io.gfile.glob(
+        '/home/alvaro/Desktop/video2tfrecord/example/train/*.tfrecords')
+
+dataset = load_data_tfrecord(files, 5, False)
 
 def eval_gen():
     for (hand_seq, face_seq, triangle_data, centroids, label, video_name_list, triangle_stream) in dataset:
-        yield [hand_seq[:, 0], hand_seq[:, 1], face_seq, triangle_data], video_name_list[0][0].numpy()
+        yield [hand_seq[:, 0], hand_seq[:, 1], face_seq, triangle_data], label
 
-model = load_model('/home/alvaro/Documentos/model')
-class_vocab = pd.read_csv('./src/class_id_correspondence.csv')
 
-for data, video_name in eval_gen():
-    probabilities = model.predict(data)[0]
-    for i in np.argsort(probabilities)[::-1]:
-        print(f"  {class_vocab.iloc[i]['EN']}: {probabilities[i] * 100:5.2f}%")
-    
+cnn_model = cnn_lstm.get_cnn_model(False)
+recurrent_model = cnn_lstm.get_recurrent_model(0.0001, cnn_model)
+
+recurrent_model.load_weights(
+            '/home/alvaro/Desktop/multi-cue-sign-language/src/models/efficient_net_b0_fine_v2/')
+
+class_vocab = pd.read_csv('./src/utils/class_id_correspondence.csv')
+
+result = recurrent_model.evaluate(dataset)
+
+for data, label in eval_gen():
+    probabilities = recurrent_model.predict(data)[0]
+    class_prediction = np.argsort(probabilities)[::-1]
+
+    print(f"  {class_vocab.iloc[i]['EN']}: {probabilities[i] * 100:5.2f}%")
+
--- src/read_dataset.py
+++ src/read_dataset.py
@@ -14,7 +14,50 @@ def get_image(img, width, height):
     return image
 
 
-def read_tfrecord(example_proto):
+def read_tfrecord_test(example_proto):
+    face = []
+    hand_1 = []
+    hand_2 = []
+    triangle_data = []
+
+    for image_count in range(16):
+        face_stream = 'face/' + str(image_count)
+        hand_1_stream = 'hand_1/' + str(image_count)
+        hand_2_stream = 'hand_2/' + str(image_count)
+        triangle_stream = 'triangle_data/' + str(image_count)
+
+        feature_dict = {
+            face_stream: tf.io.FixedLenFeature([], tf.string),
+            hand_1_stream: tf.io.FixedLenFeature([], tf.string),
+            hand_2_stream: tf.io.FixedLenFeature([], tf.string),
+            triangle_stream: tf.io.VarLenFeature(tf.float32),
+            'height': tf.io.FixedLenFeature([], tf.int64),
+            'width': tf.io.FixedLenFeature([], tf.int64),
+            'label': tf.io.FixedLenFeature([], tf.int64)
+        }
+
+        features = tf.io.parse_single_example(
+            example_proto, features=feature_dict)
+
+        triangle_data.append(tf.squeeze(tf.reshape(
+            features[triangle_stream].values, (1, 13))))
+
+        width = tf.cast(features['width'], tf.int32)
+        height = tf.cast(features['height'], tf.int32)
+
+        face_image = get_image(features[face_stream], width, height)
+        hand_1_image = get_image(features[hand_1_stream], width, height)
+        hand_2_image = get_image(features[hand_2_stream], width, height)
+
+        face.append(face_image)
+        hand_1.append(hand_1_image)
+        hand_2.append(hand_2_image)
+        label = tf.cast(features['label'], tf.int32)
+
+    return (hand_1, hand_2, face, triangle_data), label
+
+
+def read_tfrecord_train(example_proto):
     face = []
     hand_1 = []
     hand_2 = []
@@ -78,24 +121,32 @@ def filter_func(hands, face, triangle_data, centroids, label, video_name, triang
     return tf.math.less(label, 20)
 
 
-def load_dataset(tf_record_path):
+def load_dataset(tf_record_path, train):
     raw_dataset = tf.data.TFRecordDataset(tf_record_path)
-    parsed_dataset = raw_dataset.map(read_tfrecord)
+    if train:
+        parsed_dataset = raw_dataset.map(read_tfrecord_train)
+    else:
+        parsed_dataset = raw_dataset.map(read_tfrecord_test)
+
     return parsed_dataset
 
 
-def prepare_for_training(ds, batch_size, shuffle_buffer_size=300):
+def prepare_data(ds, batch_size, train, shuffle_buffer_size=300):
     # ds.cache() # I can remove this to don't use cache or use cocodata.tfcache
 
-    ds = ds.repeat().shuffle(buffer_size=shuffle_buffer_size).batch(
-        batch_size).prefetch(tf.data.experimental.AUTOTUNE)
+    if train:
+        ds = ds.repeat().shuffle(buffer_size=shuffle_buffer_size).batch(
+            batch_size).prefetch(tf.data.experimental.AUTOTUNE)
+    else:
+        ds = ds.shuffle(buffer_size=shuffle_buffer_size).batch(
+            batch_size)
 
     return ds
 
 
-def load_data_tfrecord(tfrecord_path, batch_size):
-    dataset = load_dataset(tfrecord_path)
+def load_data_tfrecord(tfrecord_path, batch_size, train=True):
+    dataset = load_dataset(tfrecord_path, train)
     # dataset = dataset.filter(filter_func) # use this to get a small amount of classes
 
-    dataset = prepare_for_training(dataset, batch_size)
+    dataset = prepare_data(dataset, batch_size, train)
     return dataset
