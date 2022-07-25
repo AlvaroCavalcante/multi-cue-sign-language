afafd17fced36b1111e6b110d5c94bdf998fef2f
alvaro leandro <leandro0807@live.com>
2022-07-24 23:40:10 -0300

    changing logs and traning new model

--- src/cnn_lstm.py
+++ src/cnn_lstm.py
@@ -123,7 +123,7 @@ def train_cnn_lstm_model(train_files, eval_files, epochs, batch_size, learning_r
     early_stop = EarlyStopping(monitor="val_loss", patience=3)
 
     callbacks_list = [
-        ModelCheckpoint('/home/alvaro/Desktop/multi-cue-sign-language/src/models/cnn_lstm_triangle/', monitor='val_accuracy',
+        ModelCheckpoint('/home/alvaro/Desktop/multi-cue-sign-language/src/models/new_cnn_lstm_tri_fine/', monitor='val_accuracy',
                         verbose=1, save_best_only=True, save_weights_only=True),
         LearningRateScheduler(lr_scheduler.lr_time_based_decay, verbose=1),
         tensorboard_callback,
@@ -138,6 +138,7 @@ def train_cnn_lstm_model(train_files, eval_files, epochs, batch_size, learning_r
             '/home/alvaro/Desktop/multi-cue-sign-language/src/models/cnn_lstm_fine_v3/').expect_partial()
 
     if tune_model:
+        print('Training model using keras tuner')
         tuner = model_tuner.get_tuner_instance()
         tuner.search(x=train_gen(dataset),
                      steps_per_epoch=train_steps,
@@ -146,7 +147,8 @@ def train_cnn_lstm_model(train_files, eval_files, epochs, batch_size, learning_r
                      validation_steps=val_steps,
                      callbacks=callbacks_list)
     else:
-        recurrent_model.fit(train_gen(dataset),
+        print('Training model')
+        rnn_model.fit(train_gen(dataset),
                             steps_per_epoch=train_steps,
                             epochs=epochs,
                             validation_data=eval_gen(dataset_eval),
@@ -161,8 +163,8 @@ if __name__ == '__main__':
     eval_files = tf.io.gfile.glob(
         '/home/alvaro/Desktop/video2tfrecord/example/val_norm/*.tfrecords')
 
-    epochs = 30
+    epochs = 20
     batch_size = 20
     learning_rate = 0.001
     train_cnn_lstm_model(train_files, eval_files, epochs,
-                         batch_size, learning_rate, False)
+                         batch_size, learning_rate, False, False)
