1b6369236b02e37356ddea40469163232509bdc4
alvaro leandro <leandro0807@live.com>
2023-01-17 22:24:28 -0300

    training hands and triangle model

--- src/cnn_lstm.py
+++ src/cnn_lstm.py
@@ -29,13 +29,14 @@ TRIANGLE_FIG_WIDTH, TRIANGLE_FIG_HEIGHT = 128, 128
 
 
 def get_recurrent_model(learning_rate):
-    face_cnn = get_cnn_model(FACE_WIDTH, FACE_HEIGHT, 'face', fine_tune=True)
-    face_model = rnn_models.get_face_rnn_model(face_cnn, learning_rate, optimizer='SGD')
+    # face_cnn = get_cnn_model(FACE_WIDTH, FACE_HEIGHT, 'face', fine_tune=True)
+    # face_model = rnn_models.get_face_rnn_model(
+    #     face_cnn, learning_rate, optimizer='SGD')
 
-    # hands_cnn = get_cnn_model(HAND_WIDTH, HAND_HEIGHT, 'hands', fine_tune=True)
-    # hands_model = rnn_models.get_hands_rnn_model(hands_cnn, learning_rate, optimizer='SGD')
+    hands_cnn = get_cnn_model(HAND_WIDTH, HAND_HEIGHT, 'hands')
+    hands_model = rnn_models.get_hands_rnn_model(hands_cnn, learning_rate)
 
-    # triangle_model = rnn_models.get_triangle_rnn_model(learning_rate)
+    triangle_model = rnn_models.get_triangle_rnn_model(learning_rate)
 
     # triangle_fig_model = rnn_models.get_triangle_figure_rnn_model(
     #     cnn_model, learning_rate)
@@ -43,26 +44,26 @@ def get_recurrent_model(learning_rate):
     # triangle_fig_model.load_weights(
     #     '/home/alvaro/Desktop/multi-cue-sign-language/src/models/new_tri_fig_mobile_fine_v1/').expect_partial()
 
-    face_model.load_weights(
-        '/home/alvaro/Desktop/multi-cue-sign-language/src/models/step1_face_csl_fine/').expect_partial()
+    # face_model.load_weights(
+    #     '/home/alvaro/Desktop/multi-cue-sign-language/src/models/step1_face_csl_fine/').expect_partial()
 
-    # hands_model.load_weights(
-    #     '/home/alvaro/Desktop/multi-cue-sign-language/src/models/step1_hands_csl_fine/').expect_partial()
+    hands_model.load_weights(
+        '/home/alvaro/Desktop/multi-cue-sign-language/src/models/step1_hands_csl_fine_v2/').expect_partial()
 
-    # concat_layers = Concatenate()([
-    #     hands_model.layers[-2].output, triangle_model.layers[-2].output, face_model.layers[-2].output])
+    concat_layers = Concatenate()([
+        hands_model.layers[-2].output, triangle_model.layers[-2].output])
 
-    # output = Dense(NUMBER_OF_CLASSES, activation='softmax')(face_model.layers[-2].output)
+    output = Dense(NUMBER_OF_CLASSES, activation='softmax')(concat_layers)
 
-    # rnn_model = keras.Model(
-    #     [face_model.input], output)
+    rnn_model = keras.Model(
+        [hands_model.input, triangle_model.input], output)
 
-    # rnn_model.compile(
-    #     loss='sparse_categorical_crossentropy', optimizer=tf.keras.optimizers.Adam(learning_rate=learning_rate), metrics=['accuracy']
-    # )
+    rnn_model.compile(
+        loss='sparse_categorical_crossentropy', optimizer=tf.keras.optimizers.Adam(learning_rate=learning_rate), metrics=['accuracy']
+    )
 
-    print(face_model.summary())
-    return face_model
+    print(rnn_model.summary())
+    return rnn_model
 
 
 def get_cnn_model(width: int, height: int, prefix_name: str, fine_tune=False):
@@ -98,9 +99,9 @@ def train_cnn_lstm_model(train_files, eval_files, epochs, batch_size, learning_r
     tensorboard_callback = keras.callbacks.TensorBoard(log_dir=logdir)
 
     callbacks_list = [
-        ModelCheckpoint('/home/alvaro/Desktop/multi-cue-sign-language/src/models/step1_face_csl_fine_v2/', monitor='val_accuracy',
+        ModelCheckpoint('/home/alvaro/Desktop/multi-cue-sign-language/src/models/step2_hands_triangle_csl/', monitor='val_accuracy',
                         verbose=1, save_best_only=True, save_weights_only=True),
-        LearningRateScheduler(lr_scheduler.lr_asc_desc_decay, verbose=1),
+        # LearningRateScheduler(lr_scheduler.lr_asc_desc_decay, verbose=1),
         tensorboard_callback,
         # EarlyStopping(monitor="val_loss", patience=3)
     ]
@@ -151,9 +152,9 @@ if __name__ == '__main__':
     eval_files = tf.io.gfile.glob(
         '/home/alvaro/Desktop/video2tfrecord/results/test/*.tfrecords')
 
-    epochs = 30
+    epochs = 40
     batch_size = 30
-    learning_rate = 1e-5
+    learning_rate = 1e-3
     train_cnn_lstm_model(train_files, eval_files, epochs,
                          batch_size, learning_rate,
                          load_weights=False,
--- src/read_dataset.py
+++ src/read_dataset.py
@@ -74,9 +74,9 @@ def read_tfrecord_test(example_proto):
         keypoint_stream = 'keypoint/' + str(image_count)
 
         feature_dict = {
-            face_stream: tf.io.FixedLenFeature([], tf.string),
-            # hand_1_stream: tf.io.FixedLenFeature([], tf.string),
-            # hand_2_stream: tf.io.FixedLenFeature([], tf.string),
+            # face_stream: tf.io.FixedLenFeature([], tf.string),
+            hand_1_stream: tf.io.FixedLenFeature([], tf.string),
+            hand_2_stream: tf.io.FixedLenFeature([], tf.string),
             # triangle_fig_stream: tf.io.FixedLenFeature([], tf.string),
             triangle_stream: tf.io.VarLenFeature(tf.float32),
             moviment_stream: tf.io.VarLenFeature(tf.float32),
@@ -104,18 +104,18 @@ def read_tfrecord_test(example_proto):
         width = tf.cast(features['width'], tf.int32)
         height = tf.cast(features['height'], tf.int32)
 
-        face_image = get_image(features[face_stream], width, height)
-        # hand_1_image = get_image(features[hand_1_stream], width, height)
-        # hand_2_image = get_image(features[hand_2_stream], width, height)
+        # face_image = get_image(features[face_stream], width, height)
+        hand_1_image = get_image(features[hand_1_stream], width, height)
+        hand_2_image = get_image(features[hand_2_stream], width, height)
         # triangle_fig = get_image(features[triangle_fig_stream], 128, 128, True)
 
-        face.append(face_image)
-        # hands.append(tf.concat([hand_1_image, hand_2_image], axis=1))
+        # face.append(face_image)
+        hands.append(tf.concat([hand_1_image, hand_2_image], axis=1))
         # triangle_figures.append(triangle_fig)
 
     label = tf.cast(features['label'], tf.int32)
 
-    return (face), label
+    return (hands, triangle_data), label
 
 
 def read_tfrecord_train(example_proto):
@@ -139,9 +139,9 @@ def read_tfrecord_train(example_proto):
         keypoint_stream = 'keypoint/' + str(image_count)
 
         feature_dict = {
-            face_stream: tf.io.FixedLenFeature([], tf.string),
-            # hand_1_stream: tf.io.FixedLenFeature([], tf.string),
-            # hand_2_stream: tf.io.FixedLenFeature([], tf.string),
+            # face_stream: tf.io.FixedLenFeature([], tf.string),
+            hand_1_stream: tf.io.FixedLenFeature([], tf.string),
+            hand_2_stream: tf.io.FixedLenFeature([], tf.string),
             # triangle_fig_stream: tf.io.FixedLenFeature([], tf.string),
             triangle_stream: tf.io.VarLenFeature(tf.float32),
             moviment_stream: tf.io.VarLenFeature(tf.float32),
@@ -170,24 +170,24 @@ def read_tfrecord_train(example_proto):
         width = tf.cast(features['width'], tf.int32)
         height = tf.cast(features['height'], tf.int32)
 
-        face_image = get_image(features[face_stream], width, height)
-        # hand_1_image = get_image(features[hand_1_stream], width, height)
-        # hand_2_image = get_image(features[hand_2_stream], width, height)
+        # face_image = get_image(features[face_stream], width, height)
+        hand_1_image = get_image(features[hand_1_stream], width, height)
+        hand_2_image = get_image(features[hand_2_stream], width, height)
         # triangle_fig = get_image(features[triangle_fig_stream], 128, 128, True)
 
-        face_image = transform_image(
-            face_image, width, apply_proba_dict, range_aug_dict, seed)
-        # hand_1_image = transform_image(
-        #     hand_1_image, width, apply_proba_dict, range_aug_dict, seed, True)
-        # hand_2_image = transform_image(
-        #     hand_2_image, width, apply_proba_dict, range_aug_dict, seed, True)
+        # face_image = transform_image(
+        #     face_image, width, apply_proba_dict, range_aug_dict, seed)
+        hand_1_image = transform_image(
+            hand_1_image, width, apply_proba_dict, range_aug_dict, seed, True)
+        hand_2_image = transform_image(
+            hand_2_image, width, apply_proba_dict, range_aug_dict, seed, True)
 
-        face.append(face_image)
-        # hands.append(tf.concat([hand_1_image, hand_2_image], axis=1))
+        # face.append(face_image)
+        hands.append(tf.concat([hand_1_image, hand_2_image], axis=1))
         # triangle_figures.append(triangle_fig)
         label = tf.cast(features['label'], tf.int32)
 
-    return (face), label
+    return (hands, triangle_data), label
 
 
 def filter_func(hands, face, triangle_data, centroids, label, video_name, triangle_stream_arr):
